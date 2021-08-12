805e76a0ff3f6f6ea17cc33149e61319c381f12c
Viki-Bo <borodienko.v@gmail.com>
2021-08-12 02:58:25 -0700

    initial project_services_section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+
+const AboutUs = () => {
+    return (
+        <>
+        <AboutSection />
+        <ServicesSection />
+        </>
+    );
+};
+
+export default AboutUs;
