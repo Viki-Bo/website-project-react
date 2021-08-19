452dc3581710f57f905b8291862a883faa9659dc
Viki-Bo <borodienko.v@gmail.com>
2021-08-19 06:59:12 -0700

    add nav component

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+import {About} from '../styles';
 
 const FaqSection = () => {
     return (
-        <div className="faq">
+        <Faq>
             <h2>Any Questions? <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
@@ -11,6 +13,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet consectetur 
                         adipisicing elit. Magni, voluptate!
                     </p>
+                    <div className="faq-line"></div>
                 </div>
             </div>
             <div className="question">
@@ -20,6 +23,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet consectetur 
                         adipisicing elit. Magni, voluptate!
                     </p>
+                    <div className="faq-line"></div>
                 </div>
             </div>
             <div className="question">
@@ -29,6 +33,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet consectetur 
                         adipisicing elit. Magni, voluptate!
                     </p>
+                    <div className="faq-line"></div>
                 </div>
             </div>
             <div className="question">
@@ -38,10 +43,38 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet consectetur 
                         adipisicing elit. Magni, voluptate!
                     </p>
+                    <div className="faq-line"></div>
                 </div>
             </div>
-        </div>
+        </Faq>
     );
 };
+
+const Faq = styled(About)`
+    display: block;
+    span {
+        display: block;
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }
+`;
 
 export default FaqSection;
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
       background: #1b1b1b;
+      font-family: 'Inter', sans-serif;
     }
 
   button {
@@ -21,6 +22,7 @@ const GlobalStyle = createGlobalStyle`
       background: transparent;
       color: white;
       transition: all 0.5s ease;
+      font-family: 'Inter', sans-serif;
       &:hover {
           background-color: #23d997;
           color: white;
@@ -38,6 +40,7 @@ const GlobalStyle = createGlobalStyle`
 
     h4 {
         font-weight: bold;
+        font-size: 2rem;
     }
 
     a {
